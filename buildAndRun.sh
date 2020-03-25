8bde21006399082aaa969d08a994da1fb0bdaf52
Steve Pham <spham@deloitte.com>
2020-03-26 00:45:42 +0800

    Change port to 8080

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,6 +1,6 @@
 
 mvn clean package
 
-docker build -t hellomule4 .
+docker build -t stevehungpham/demoepf .
 
-docker run -it --rm -p 8088:8088 hellomule4 
+docker run -it --rm -p 8088:8088 stevehungpham/demoepf 
